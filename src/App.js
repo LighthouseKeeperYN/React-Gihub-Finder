2d0f875dd9e70cc5b929a27703117fecc10bd055
LighthouseKeeperYN <freesaber@yandex.ru>
2019-12-10 14:31:00 +0300

    implement user data page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -12,8 +13,10 @@ import About from './components/pages/About';
 class App extends Component {
   state = {
     users: [],
+    repos: [],
+    user: {},
     loading: false,
-    alert: null
+    alert: null,
   }
 
   searchUsers = async (text) => {
@@ -32,8 +35,24 @@ class App extends Component {
     setTimeout(() => this.setState({ alert: null }), 3000);
   }
 
+  getUser = async (userName) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${userName}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
+  getUserRepos = async (userName) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${userName}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
   render() {
-    const { users, loading, alert } = this.state;
+    const { users, loading, alert, user } = this.state;
 
     return (
       <Router>
@@ -58,6 +77,10 @@ class App extends Component {
               )} />
 
               <Route exact path='/about' component={About} />
+
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} />
+              )} />
             </Switch>
           </div>
 
